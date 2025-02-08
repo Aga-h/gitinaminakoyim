8ca04524db6e7f13796d10342d8ea8b0ff34c556
Aga-h <162789899+Aga-h@users.noreply.github.com>
2025-02-08 14:44:11 +0300

    Array

---
+++ Assets/Scripts/BoardPlaces.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardPlaces : MonoBehaviour
+{
+    public static string[] buyableProperties = {"Arikan","Bilginer","Lojmanlar","Hasret","AnaBina","MudurOdasi","Antoinette","Pulak","EdebiyatZumresi","Temple","MudurMalikane","Sedefoglu","Prep","Gastronomi","FagotOdasi","Revir","Tekcan","Helivel","Lab","EdebiyatBinasi","PrepBinasi","FenBinasi" };
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame\
+    void Update()
+    {
+        
+    }
+}
